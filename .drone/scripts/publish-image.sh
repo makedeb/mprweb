036aa7ed8bcef45e52ad093f5f277408b7de4786
Hunter Wittenborn <hunter@hunterwittenborn.com>
2021-11-13 19:34:49 -0600

    Fixed casing on Drone secret used to provide MPR secret to published release [TEST SKIP]

--- .drone/scripts/publish-image.sh
+++ .drone/scripts/publish-image.sh
@@ -3,7 +3,7 @@ set -ex
 
 # Set up env file.
 sed -i \
-    -e "s|DB_PASSWORD='mpr'|DB_PASSWORD='${MPR_DB_PASSWORD}'|" \
+    -e "s|DB_PASSWORD='mpr'|DB_PASSWORD='${mpr_db_password}'|" \
     -e "s|AURWEB_FASTAPI_PREFIX='https://localhost:8444'|AURWEB_FASTAPI_PREFIX='https://${mpr_url}'|" \
     conf/docker.env
 
