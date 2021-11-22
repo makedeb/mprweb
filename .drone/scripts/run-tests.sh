351986905d0cfe6bd40b7bf3636679b9ef9f9c19
Hunter Wittenborn <hunter@hunterwittenborn.com>
2021-11-21 21:24:19 -0600

    Updated unit test script to match upstream

--- .drone/scripts/run-tests.sh
+++ .drone/scripts/run-tests.sh
@@ -12,22 +12,17 @@ export AURWEB_FASTAPI_PREFIX CONFIG_FILE
 ./docker/scripts/install-deps.sh
 ./docker/scripts/install-python-deps.sh
 
-useradd -U -d /aurweb -c 'AUR User' aur
+useradd -U -d /aurweb -c 'MPR User' mpr
 
 ./docker/mariadb-entrypoint.sh
 (cd /usr && /usr/bin/mysqld_safe --datadir=/var/lib/mysql) &
 until : > /dev/tcp/127.0.0.1/3306; do sleep 1s; done
 ./docker/test-mysql-entrypoint.sh
-./docker/test-sqlite-entrypoint.sh
 
 make -C po all install
-
-python -m aurweb.initdb
-AUR_CONFIG='conf/config.sqlite' python -m aurweb.initdb
-
 make -C test clean
-make -C test sh pytest
-AUR_CONFIG='conf/config.sqlite' make -C test pytest
+make -C test sh
+pytest
 make -C test coverage
 
 flake8 --count aurweb
