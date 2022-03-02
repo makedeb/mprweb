38fcac790aa3700ff8c528a1f136927b96c67a5a
Hunter Wittenborn <hunter@hunterwittenborn.com>
2022-03-02 00:51:04 -0600

    Ported '/pkgbase/{name}/request'

--- media/js/typeahead-pkgbase-request.js
+++ media/js/typeahead-pkgbase-request.js
@@ -24,8 +24,6 @@ document.addEventListener('DOMContentLoaded', function() {
     const input = document.getElementById('id_merge_into');
     const form = document.getElementById('request-form');
     const type = "suggest-pkgbase";
-
-    typeahead.init(type, input, form, false);
 });
 
 // Bind the change event here, otherwise we have to inline javascript,
