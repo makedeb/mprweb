4c3cfd0c23522e694b9b9d99b68f312891a919cd
Hunter Wittenborn <hunter@hunterwittenborn.com>
2022-03-04 00:51:20 -0600

    Attempted to fix unit tests

--- .drone/scripts/run-tests.sh
+++ .drone/scripts/run-tests.sh
@@ -4,19 +4,18 @@ if echo "${DRONE_COMMIT_MESSAGE}" | grep -q 'TEST SKIP'; then
     exit 0
 fi
 
-# Whenever upstream changes are merged in, the 'gitlab-ci.yml' file should be checked to ensure these commands are equivilant to those there.
 set -ex
 AUR_CONFIG='conf/config'
 DB_HOST='localhost'
 TEST_RECURSION_LIMIT='10000'
 CURRENT_DIR="$(pwd)"
 LOG_CONFIG='logging.test.conf'
-PATH="$HOME/.poetry/bin:${PATH}"
-export AUR_CONFIG DB_HOST TEST_RECURSION_LIMIT CURRENT_DIR LOG_CONFIG PATH
+PYTHONPATH='/aurweb:/aurweb/app'
+export AUR_CONFIG DB_HOST TEST_RECURSION_LIMIT CURRENT_DIR LOG_CONFIG PYTHONPATH
 
 ./docker/scripts/install-deps.sh
 ./docker/scripts/install-python-deps.sh
-useradd -U -d /aurweb -c 'AUR User' aur
+useradd -U -d /aurweb -c 'AUR User' mpr
 ./docker/mariadb-entrypoint.sh
 (cd '/usr' && /usr/bin/mysqld_safe --datadir='/var/lib/mysql') &
 until : > /dev/tcp/127.0.0.1/3306; do sleep 1s; done
@@ -29,9 +28,6 @@ make -C test clean
 make -C test sh
 pytest
 make -C test coverage
-flake8 --count aurweb
-flake8 --count test
-flake8 --count migrations
-isort --check-only aurweb
-isort --check-only test
-isort --check-only migrations
+black ./
+flake8 --count ./
+isort --check-only ./
