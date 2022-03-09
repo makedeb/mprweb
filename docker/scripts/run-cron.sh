b63bb91d1f25157e2c08965b9e68eaa41c53e455
Hunter Wittenborn <hunter@hunterwittenborn.com>
2022-03-08 22:46:38 -0600

    Fixed missing environment variables in Cron jobs
    Also added executions of commands upon container creating so it's easier to see if everything is working correctly right away.
    
    Also removed command that outputted to log file, as it didn't appear to be being used anywhere else.

--- docker/scripts/run-cron.sh
+++ docker/scripts/run-cron.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-
 cd /aurweb
 
+export AUR_CONFIG='/aurweb/conf/config'
+export PYTHONPATH='/aurweb'
+
+# Running the scripts here allows us to get a quick look at if everything is working right.
 aurweb-mkpkglists --extended
-if [ $? -eq 0 ]; then
-    echo "[$(date -u)] executed mkpkglists" >> /var/log/mkpkglists.log
-fi
+aurweb-pkgmaint
+aurweb-usermaint
+aurweb-popupdate
+aurweb-tuvotereminder
+aurweb-oodcheck
 
 exec /usr/bin/crond -nx proc
